cae3b21ddf3a48ce898dbe64f7b0e53d711f45f2
ehipluto <109985862+ehipluto@users.noreply.github.com>
2024-11-25 21:02:01 +0100

    Update matches_H2H.py

--- methods/matches/matches_H2H.py
+++ methods/matches/matches_H2H.py
@@ -1,11 +1,12 @@
 import requests, json
 
+
 def getH2H(home_team_id, away_team_id):
     url = f"https://table-tennis.sportdevs.com/matches?home_team_id=in.({home_team_id},{away_team_id})&away_team_id=in.({home_team_id},{away_team_id})"
-    payload={}
+    payload = {}
     headers = {
         'Accept': 'application/json',
-        'Authorization': f'Bearer dsBH5k-JaEybFCGkeMY2gg' 
+        'Authorization': f'Bearer dsBH5k-JaEybFCGkeMY2gg'
     }
     home = 0
     away = 0
@@ -18,15 +19,13 @@ def getH2H(home_team_id, away_team_id):
                     if elem['home_team_score']['current'] > elem['away_team_score']['current']:
                         home += 1
                     else:
-                        away +=1
+                        away += 1
 
                 elif elem['away_team_id'] == home_team_id:
                     if elem['away_team_score']['current'] > elem['home_team_score']['current']:
                         home += 1
                     else:
-                        away +=1
-
-
+                        away += 1
 
     if home == 0:
         homePer = 0
@@ -34,16 +33,13 @@ def getH2H(home_team_id, away_team_id):
     elif away == 0:
         homePer = 100
         awayPer = 0
-    else: 
-        homePer = (home/(home+away)) * 100
-        awayPer = (away/(home+away)) * 100
-    
-    return homePer, awayPer
+    else:
+        homePer = (home / (home + away)) * 100
+        awayPer = (away / (home + away)) * 100
 
+    TotalMatches = home + away
+    return homePer, awayPer, TotalMatches
 
-
-
-    
 
 home_team_i = 150130
 away_team_i = 66556
