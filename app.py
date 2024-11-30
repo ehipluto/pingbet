2b5c7eb5f98acc378c0f07995e8f2229c3b382f3
Enricomaria Di Rosolini <enricomariadr99@gmail.com>
2024-11-30 11:27:40 +0100

    db.py permette la gestione del database.

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template
 from methods.matches import matches_by_date as tday
-
+from methods.db.db import DB
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    listSet = tday.getMatchToday()
-    print("CIAO")
-    return render_template('matches.html', matches=listSet)
+    listMatches = tday.getMatchToday()
+
+    return render_template('matches.html', matches=listMatches)
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                              'favicon.ico', mimetype='image/vnd.microsoft.icon')
