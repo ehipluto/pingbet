b4acce2f5c56a33751733b11700d13e7dedeeaea
ehipluto <109985862+ehipluto@users.noreply.github.com>
2025-01-11 15:05:23 +0100

    Filtri ripristinati

--- methods/matches/matchesday.py
+++ methods/matches/matchesday.py
@@ -45,7 +45,7 @@ def update_database():
                 tournament_matches = True
 
 
-        if elem['status'] != "finished" and elem['status'] != 'live':
+        if elem['status'] != "finished" and elem['status'] != 'live' and tournament_matches:
             print("Controllo Odds")
             val = odds(elem['id'])
             if val:
