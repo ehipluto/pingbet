624d7f0786213d473bbe13863e2fe0be6558f33a
ehipluto <109985862+ehipluto@users.noreply.github.com>
2025-01-16 11:37:31 +0100

    Aggiunta TotalMatches

--- methods/matches/matches_H2H.py
+++ methods/matches/matches_H2H.py
@@ -39,8 +39,9 @@ def getH2H(home_team_id, away_team_id):
     else: 
         homePer = (home/(home+away)) * 100
         awayPer = (away/(home+away)) * 100
-    
-    return homePer, awayPer
+        
+    TotalMatches = home + away
+    return homePer, awayPer, TotalMatches
 
 
 
